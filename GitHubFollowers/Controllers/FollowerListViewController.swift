294aae7930d5997578cb49b7caa5b8c4676d6090
Patrick Nguyen <patar.nguyen@gmail.com>
2022-11-01 18:51:41 -0400

    Set up collection view

--- GitHubFollowers/Controllers/FollowerListViewController.swift
+++ GitHubFollowers/Controllers/FollowerListViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 class FollowerListViewController: UIViewController {
 
     var username: String!
+    var collectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+        getFollowers()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        //navigationController?.isNavigationBarHidden = false
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(FollowerCollectionViewCell.self, forCellWithReuseIdentifier: FollowerCollectionViewCell.reuseID)
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
+    func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { /*(followers, errorMessage)*/ result in
             switch result {
             case.success(let followers):
@@ -31,11 +68,5 @@ class FollowerListViewController: UIViewController {
 //            print("followers.count = \(followers.count)")
 //            print(followers)
         }
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        //navigationController?.isNavigationBarHidden = false
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
---
+++ GitHubFollowers/Views/GHFAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  GHFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Patrick Nguyen on 2022-10-31.
+//
+
+import UIKit
+
+class GHFAvatarImageView: UIImageView {
+
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+
+}
