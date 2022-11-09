f667b1149dcde6d97df81614038b9ad3b2323bbd
Patrick Nguyen <patar.nguyen@gmail.com>
2022-11-08 21:24:06 -0500

    add item info

---
+++ GitHubFollowers/Views/ViewControllers/GHFRepoItemViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GHFRepoItemViewController.swift
+//  GitHubFollowers
+//
+//  Created by Patrick Nguyen on 2022-11-08.
+//
+
+import UIKit
+
+class GHFRepoItemViewController: GHFItemInfoViewController {
+    
+}
+
